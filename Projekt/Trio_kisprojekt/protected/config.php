cf3fe29c8f2c7000f3d50e09b356b9a01b9f8acb
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2020-09-30 23:43:32 +0200

    Adatbáziskapcsolat adatok beállítása a config.php-ban

--- Projekt/Trio_kisprojekt/protected/config.php
+++ Projekt/Trio_kisprojekt/protected/config.php
@@ -7,9 +7,10 @@ define('DATABASE_CONTROLLER', PROTECTED_DIR.'database.php');
 define('USER_MANAGER', PROTECTED_DIR.'userManager.php');
 
 define('DB_TYPE', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'webprog2_zh');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'b2fydealqqgxz1qyzlto-mysql.services.clever-cloud.com');
+define('DB_NAME', 'b2fydealqqgxz1qyzlto');
+define('DB_USER', 'uuedf7rjecssfbx3');
+define('DB_PASS', '4hJzSYOY9nNlUwt6GYTR');
+define('DB_PORT', '3306');
 define('DB_CHARSET', 'utf8');
 ?>
