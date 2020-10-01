560d79ac1681885b47873eafc0c2f591bc559289
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2020-10-01 18:55:50 +0200

    header.php fájl létrehozása és elemek hozzáadása, a style.css és az index.php fájlok módosítása

--- Projekt/Trio_kisprojekt/index.php
+++ Projekt/Trio_kisprojekt/index.php
@@ -7,15 +7,13 @@
 <head>
 	<meta charset="utf-8">
 	<title>Trio Múzeum</title>
-
-
 	<!-- Saját CSS -->
 	<link rel="stylesheet" type="text/css" href="<?=PUBLIC_DIR.'style.css?'.date('YmdHis', filemtime(PUBLIC_DIR.'style.css'))?>">
 </head>
-<body class="bg-light text-dark">
-	<p style="font-size: 36px; text-align: center">Múzeumi weboldal</p>
+<body>
+	<header><?php include_once PROTECTED_DIR.'header.php'; ?></header>
 	<nav><?php require_once PROTECTED_DIR.'nav.php'; ?></nav>
-	<div class="container-fluid">
+	<div>
 		<content><?php require_once PROTECTED_DIR.'routing.php'; ?></content>
 		<footer><?php include_once PROTECTED_DIR.'footer.php'; ?></footer>
 	</div>
---
+++ Projekt/Trio_kisprojekt/protected/header.php
@@ -0,0 +1,5 @@
+<h1>Múzeumi weboldal</h1>
+<div id=changeStyle>
+	<button id="defaultStyle" name="defaultStyleButton" type="button">Stílus 1</button>
+	<button id="alternativeStyle" name="alternativeStyleButton" type="button">Stílus 2</button>
+</div>
