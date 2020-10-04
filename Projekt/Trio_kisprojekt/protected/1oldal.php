e02cad8c70488cf5674030133be1343f39a224c7
Zsolt Perge <zsolta500@gmail.com>
2020-10-04 15:42:04 +0200

    Kategóriaválasztó php

---
+++ Projekt/Trio_kisprojekt/protected/1oldal.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<link rel="stylesheet" href="Style.css"> 
+<title>1.Oldal</title>
+</head>
+<div><div class="center"><?php
+echo "<button id='baloldali'><a href='2,oldal.php?id=1><button name='1'>1.kat</button>";
+echo "<button id='ereklye'><a href='2,oldal.php?id=2><button name='2'>2.kat</button>";
+echo "<button id='fossil'><a href='2,oldal.php?id=3><button name='3'>3.kat</button>";
+echo "<button id='kepek'><a href='2,oldal.php?id=4><button name='4'>4.kat</button>";
+?></div></div>
+</html>
