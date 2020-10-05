1d8a18d347c0401629c6017e597c8d71d78a42c7
Patrik <dohanyos96@gmail.com>
2020-10-05 17:22:45 +0200

    informacio.php - kesz

--- Projekt/Trio_kisprojekt/protected/informacio.php
+++ Projekt/Trio_kisprojekt/protected/informacio.php
@@ -4,7 +4,29 @@
 <title>Informacio</title>
 </head>
 <body>
-    <div><h1 style="text-align:center">Állandó kiállítás jegyárai</h1></div>
+    <div>
+        <div>
+            <h1 style="text-align:center">Belépő és szolgáltatás vásárlási lehetőségek</h1>
+        </div>
+        <div style="width:800px; margin:0 auto">
+            <span>
+                <p style="width: 900;text-align: center;">
+                </br></br></br></br>
+                <h4 style="font-weight: normal;
+                            text-align: justify;">
+                    2018-ban új koncepció alapján létrehozott állandó kiállításokkal várjuk látogatóinkat: Egyiptom és az ókor művészete mellett a 18. század végéig együtt mutatjuk be az egyetemes és a magyar művészet történetét a Magyar Nemzeti Galéria Régi Magyar gyűjteményével való újraegyesítésnek köszönhetően. Az épület barokk szárnyának rekonstrukcióját követően újabb állandó kiállítások  nyílnak meg 2020-ban, ezek között lesznek a spanyol és a holland festészetet bemutató tárlatok is (1600-1800).
+                    A Szépművészeti Múzeum 1800 utáni gyűjteményéből a Magyar Nemzeti Galériában látható válogatás.
+                    A kiállítóterek légkondicionáltak, a hőmérséklet kb. 22 °C.
+                </h4>
+                </br></br></br></br>
+            </p>
+            </span>
+
+        </div>
+    </div>
+    <div>
+        <h1 style="text-align:center">Állandó kiállítás jegyárai</h1>
+    </div>
     <div style="width:800px; margin:0 auto;">
     <table style="width: 70%;" border="1" cellpadding="1">
         <tbody>
@@ -37,6 +59,34 @@
              </tr>
         </tbody>
     </table>
+    </div>
+</br></br></br></br>
+    <div style="width:800px; margin:0 auto;">
+        <div>
+            <h1 style="text-align:center">
+                Muzeum elerhetosegei
+            </h1>
+            <span>
+                1141 Budapest, Dózsa György út 2.
+            </span>
+            <div>
+                <span>
+                    Levélcím: 1342 Budapest, 62. Pf. 321.
+                </span>
+            </br>
+            </div>
+            <div>
+                <span>
+                    Tel: (+36-1) 222- 2200
+                </span>
+            </div>
+        </div>
+    </div>
+</br></br></br></br>
+    <div style="width:800px; margin:0 auto;">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2695.431227323662!2d19.095484315626912!3d47.50099257917781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741dc8ffe1cec23%3A0x803be17b0b0394d9!2zQnVkYXBlc3QsIETDs3pzYSBHecO2cmd5IMO6dCAyLCAxMTQ2!5e0!3m2!1shu!2shu!4v1601910986028!5m2!1shu!2shu"
+        width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+        </iframe>
     </div>
 </body>
 </html>
