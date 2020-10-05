8703e66ba62d42ca9eb5708ebf280a9886f1a2cd
Patrik <dohanyos96@gmail.com>
2020-10-05 16:52:42 +0200

    Informacio.php vaza

---
+++ Projekt/Trio_kisprojekt/protected/informacio.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<link rel="stylesheet" href=""> 
+<title>Informacio</title>
+</head>
+<body>
+    <div><h1 style="text-align:center">Állandó kiállítás jegyárai</h1></div>
+    <div style="width:800px; margin:0 auto;">
+    <table style="width: 70%;" border="1" cellpadding="1">
+        <tbody>
+            <tr>
+                <td><b>Gyerek jegy</b></td>
+                <td>1000 Ft</td>
+
+            </tr>
+            <tr>
+                <td><b>Diak jegy</b></td>
+                <td>2800 Ft</td>
+
+            </tr>
+            <tr>
+                <td><b>Teljes árú</b></td>
+                <td>3400 Ft</td>
+
+            </tr>
+            <tr>
+                <td><b>Nyugdijas</b></td>
+                <td>2200 Ft</td>
+
+            </tr>
+             <tr>
+                <td><b>Csoportos jegy</b>
+                 <div></div>legalább 15 db teljes árú belépő váltása esetén vehető igénybe</div>
+                </td>
+                 <td>3100 Ft / DB</td>
+
+             </tr>
+        </tbody>
+    </table>
+    </div>
+</body>
+</html>
